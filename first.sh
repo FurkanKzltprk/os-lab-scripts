ea03ea277ca12376b8b586260070b545d663c0ef
FurkanKzltprk <kiziltoprakfurkan001@gmail.com>
2026-04-09 00:50:10 +0300

    first.sh added,variables and read usage

---
+++ first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "!=!=!=!=!=!=!=!=!=!=!=!=!=!=!"
+echo "first script"
+echo "PATH=$PATH"
+echo
+echo "USER=$USER"
+echo -n "Write your name: " 
+read name
+echo "Hello $name !! " 
+exit 0
+
