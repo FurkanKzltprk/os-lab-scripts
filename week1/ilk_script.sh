9bf7b38a89946ae74256e996ad71a848e7178da5
FurkanKzltprk <kiziltoprakfurkan001@gmail.com>
2026-04-10 11:54:30 +0200

    I started writing code with VS Code and WSL:ubuntu.

---
+++ week1/ilk_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Hi Furkan , Linux lab is starting"
---
+++ week1/lab1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#read -p "What is your name ?¿ " username2
+#echo "Welcome, $username2 ! "
+
+#ask the user for their name 
+echo -n "What is your name ¿? :: "
+read username
+
+#greet the user
+
+echo "Nice to meet you $username ! Welcome to Linux Laboratory."
+
+#print current directory
+
+echo -n "You are currently working in this directory :"
+pwd
+
+
+
